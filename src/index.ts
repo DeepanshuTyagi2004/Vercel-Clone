329dd53c770d4b3f987aef03ecbc02f7519c550c
Deepanshu <113329037+Kirito04@users.noreply.github.com>
2024-02-18 15:42:56 +0530

    Step 1 completed, cloned a git repo and stored it in a object store(cloudflare R2)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import express from "express"
 import cors from "cors";
+import simpleGit from 'simple-git';
+import { generate, getAllFiles } from "./utils";
+import path from "path";
+import { uploadFile } from "./aws";
+
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/deploy", (req, res) => {
+app.post("/deploy", async (req, res) => {
   const repoUrl = req.body.repoUrl;
+  const id = generate();
+  await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
+  const files = getAllFiles(path.join(__dirname, `output/${id}`));
+  files.forEach(async (file) => {
+    await uploadFile(file.slice(__dirname.length+1), file);
+  })
   console.log(repoUrl);
-  res.json({});
+  res.json({
+    id
+  });
 })
 
 app.listen(port);
